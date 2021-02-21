4ba0dbfbf9302545a67098d3f21f1e6340a5a9db
Aniko Kopis-Horvath <ha891209@gmail.com>
2021-02-21 18:34:39 +0100

    already finish

---
+++ src/app/page/events-list/events-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Event } from 'src/app/model/event';
+import { EventService } from 'src/app/service/event.service';
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: ['./events-list.component.scss']
+})
+export class EventsListComponent implements OnInit {
+
+  eventList$: BehaviorSubject<Event[]> = this.eventService.list$;
+
+
+  constructor(
+    private eventService: EventService,
+  ) { }
+
+  ngOnInit(): void {
+    this.eventService.getAll();
+  }
+
+  onDelete(id: number): void {
+    this.eventService.remove(id).subscribe(
+      () => {
+        this.eventService.getAll();
+      }
+    );
+  }
+
+}
---
+++ src/app/service/event.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { Event } from '../model/event';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  serverUrl: string = `http://localhost:3000/list`;
+
+  list$: BehaviorSubject<Event[]> = new BehaviorSubject<Event[]>([]);
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): void {
+    this.list$.next([]);
+    this.http.get<Event[]>(this.serverUrl).subscribe(events => this.list$.next(events));
+  }
+
+  get(id: number): Observable<Event> {
+    return Number(id) === 0 ? of(new Event()) : this.http.get<Event>(`${this.serverUrl}/${Number(id)}`);
+  }
+
+  update(event: Event): Observable<Event> {
+    return this.http.patch<Event>(`${this.serverUrl}/${event.id}`, event);
+  }
+
+  create(event: Event): Observable<Event> {
+    return this.http.post<Event>(this.serverUrl, event);
+  }
+
+  remove(id: number): Observable<Event> {
+    return this.http.delete<Event>(`${this.serverUrl}/${id}`);
+  }
+
+}
